275f5e88035e9931aaade6af2c7e7ec6dd9d0573
Aowss Ibrahim <aowss@yahoo.com>
2024-09-06 17:44:44 -0400

    Using popup for login and logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,39 @@
-<template>
-  <el-menu class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff">
-    <el-menu-item v-if="isAuthenticated"><router-link to="/mail">Mail</router-link></el-menu-item>
-    <el-menu-item v-if="isAuthenticated"><SignOutButton/></el-menu-item>
-    <el-menu-item v-if="!isAuthenticated"><SignInButton/></el-menu-item>
-  </el-menu>
-</template>
-
 <script setup lang="ts">
+import { useMsal } from '../composition-api/useMsal';
+import { loginRequest } from "../config/authConfig";
 import { useIsAuthenticated } from '../composition-api/useIsAuthenticated';
-import SignInButton from "./SignInButton.vue";
-import SignOutButton from "./SignOutButton.vue";
+import WelcomeName from '@/components/WelcomeName.vue'
 
 const isAuthenticated = useIsAuthenticated();
+
+const { instance } = useMsal();
+
+const loginPopup = () => instance.loginPopup(loginRequest);
+// const loginRedirect = () => instance.loginRedirect(loginRequest);
+const logoutPopup = () => instance.logoutPopup({ mainWindowRedirectUri: "/" });
+// const logoutRedirect = () => instance.logoutRedirect();
 </script>
+
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff">
+    <el-menu-item v-if="isAuthenticated">
+      <WelcomeName />
+    </el-menu-item>
+    <el-menu-item v-if="isAuthenticated">
+      <router-link to="/mail">Mail</router-link>
+    </el-menu-item>
+    <el-menu-item v-if="isAuthenticated">
+      <el-button type="contrast" @click="logoutPopup">Sign Out</el-button>
+    </el-menu-item>
+    <el-menu-item v-if="!isAuthenticated">
+      <el-button type="contrast" @click="loginPopup">Sign In</el-button>
+    </el-menu-item>
+  </el-menu>
+</template>
 
 <style>
 a {
     text-decoration: none;
     color: #fff;
-}
-#authButton {
-    margin-left: auto;
 }
 </style>
